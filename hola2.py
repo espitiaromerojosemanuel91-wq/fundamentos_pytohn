84254dde7d34648bef61137e178db404f8dfb6fa
jose <espitiaromerojosemanuel91gmail.com>
2026-05-04 12:38:43 -0500

    Mi primer commit

---
+++ hola2.py
@@ -0,0 +1,24 @@
+# operradores de arismeticos
+
+a= 10
+b= 5
+
+# suma 
+suma = a + b
+print("la summa de a y b es:",suma )
+
+# resta
+resta = a - b   
+print("la resta de a y b es:",resta)
+
+# multiplicacion
+multiplicacion = a * b
+print("la multiplicacion de a y b es:",multiplicacion)  
+
+# division
+division = a / b     
+print("la division de a y b es:",division)
+# modulo
+modulo = a % b
+print("el modulo de a y b es:",modulo)
+
